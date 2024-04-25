990387ba977fb97923952baed0a26c82a5287589
ddavid-almeida <david.tecnico20@gmail.com>
2024-04-25 12:49:51 -0300

    Botoes Sociais

---
+++ teste.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-...." crossorigin="anonymous" />
+
+        <link href="style.css" rel="stylesheet">
+        <title>botoes sociais</title>
+    </head>
+
+    <body>
+
+        <ul>
+            <a href="#">
+                <li style="--cor1: #FB3997; --cor2: #FECF12;">
+                    <span class="icon"><i class="fa-brands fa-instagram"></i></span>
+                    <span class="text">Instagram</span>
+                </li>
+            </a>
+
+            <a href="#">
+                <li style="--cor1: #09C5FF; --cor2: #000000;">
+                    <span class="icon"><i class="fa-brands fa-linkedin"></i></span>
+                    <span class="text">Linkedin</span>
+                </li>
+            </a>
+
+            <a href="#">
+                <li style="--cor1: #43FE84; --cor2: #2DBC5D;">
+                    <span class="icon"><i class="fa-brands fa-whatsapp"></i></span>
+                    <span class="text">Whatsapp</span>
+                </li>
+            </a>
+
+            <a href="#">
+                <li style="--cor1: #09C5FF; --cor2: #0986FF;">
+                    <span class="icon"><i class="fa-brands fa-facebook"></i></span>
+                    <span class="text">Facebook</span>
+                </li>
+            </a>
+        </ul>
+
+    </body>
+</html>
