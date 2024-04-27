61247e32218cb6002d34b99dd4ff59422ff803f2
ddavid-almeida <david.tecnico20@gmail.com>
2024-04-27 03:45:02 -0300

    efeito ripple no botao

---
+++ 03 - botao_ripple/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-...." crossorigin="anonymous" />
+
+        <link href="style.css" rel="stylesheet">
+        <title>Efeito Ripple</title>
+    </head>
+
+    <body>
+
+        <a href="#" class="button"><span>Login</span></a>
+
+        <script src="effects.js"></script>
+
+    </body>
+</html>
